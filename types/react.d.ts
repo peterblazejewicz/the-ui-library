7d74fed7bb2b00b10fa6a040497b99b6fb6b8688
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2017-11-07 21:38:10 +0100

    Fix TSC compiler failure with React 16.0

---
+++ types/react.d.ts
@@ -0,0 +1,8 @@
+import { Key, ReactNode } from "react";
+import React from "react";
+declare module "react" {
+  export interface ReactPortal {
+    key: Key | null;
+    children: ReactNode;
+  }
+}
