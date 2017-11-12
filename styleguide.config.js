de92fcc8c0071f69c15b35c5722171d01b4dfc7d
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2017-11-12 18:12:19 +0100

    Modify a path used to document compnent usage
    Returns the ES6 import path

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,3 +1,19 @@
+const path = require('path');
+const pkg = require('./package.json');
+
+/**
+ * Compute the path for component header
+ * Here returns ES6 import
+ * @param {any} componentPath
+ * @returns
+ */
+const getComponentPathLine = (componentPath) => {
+  const extname = path.extname(componentPath);
+  const name = path.basename(componentPath, extname);
+  const dir = `${pkg.name}/components`;
+  return `import { ${name} } from '${dir}';`;
+}
+
 module.exports = {
   components: 'src/components/**/*.{ts,tsx}',
   ignore: [
@@ -8,6 +24,7 @@ module.exports = {
     '**/*.spec.jsx',
     '**/index.ts',
   ],
+  getComponentPathLine,
   propsParser: require('react-docgen-typescript').withDefaultConfig().parse,
   webpackConfig: require('react-scripts-ts/config/webpack.config.dev.js'),
   showSidebar: true,
