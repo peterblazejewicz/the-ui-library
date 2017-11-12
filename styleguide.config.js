5c458ba02027383ee925eb7b564a1dd06048fa97
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2017-11-12 15:06:12 +0100

    Exclude index files from docs

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,6 +1,14 @@
 module.exports = {
   components: 'src/components/**/*.{ts,tsx}',
+  ignore: [
+    '**/__tests__/**',
+    '**/*.test.js',
+    '**/*.test.jsx',
+    '**/*.spec.js',
+    '**/*.spec.jsx',
+    '**/index.ts',
+  ],
   propsParser: require('react-docgen-typescript').withDefaultConfig().parse,
   webpackConfig: require('react-scripts-ts/config/webpack.config.dev.js'),
-  showSidebar: true
-}
+  showSidebar: true,
+};
